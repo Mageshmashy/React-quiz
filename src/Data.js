472df91aca801aa916e1f3282971be3733670ade
magesh s <magesh.s@eviden.com>
2024-07-01 19:00:54 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "Which device is require for data connection ?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which continent have largest number of countries ?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North America",
+        option4: "Africa",
+        ans: 4,
+    }, 
+    {
+        question: "Junk email is also called ?",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archived",
+        option4: "Bin",
+        ans: 1,
+    }, 
+    {
+        question: "A computer cannot reboot if it does not have the ?",
+        option1: "Application Software",
+        option2: "Interner",
+        option3: "Operating System",
+        option4: "Mouse",
+        ans: 3
+    },
+    {
+        question: "First page of website is termed as ?",
+        option1: "Index Page",
+        option2: "Homepage",
+        option3: "Sitemap",
+        option4: "HTML Page",
+        ans: 2
+    }
+];
